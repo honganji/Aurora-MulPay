d5589367b854b93e822fcd4cacc7ff60bd5e9062
github honganji <yujicavs@icloud.com>
2023-06-07 06:45:28 +0900

    complete contract and test

---
+++ packages/contract/hardhat.config.ts
@@ -0,0 +1,19 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
+require("dotenv").config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    testnet_aurora: {
+      url: "https://testnet.aurora.dev",
+      accounts:
+        process.env.AURORA_PRIVATE_KEY !== undefined
+          ? [process.env.AURORA_PRIVATE_KEY]
+          : [],
+    },
+  },
+};
+
+export default config;
